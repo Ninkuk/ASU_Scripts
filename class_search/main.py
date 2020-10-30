64d790855a8df73381e1cf2d225b4465593f8c7f
Ninad Kulkarni <ninadk03@gmail.com>
2020-10-30 08:55:02 -0700

    Formatted the menu design. Implemented subject and course number menu inputs
    Signed-off-by: ninkuk <ninadk03@gmail.com>

--- class_search/main.py
+++ class_search/main.py
@@ -3,9 +3,15 @@ from datetime import datetime
 
 def print_menu():
     selected_option = -1
-    print("\nWelcome to the ASU scripts!\n___________________________")
+
+    print("\n-------------------------------")
+    print("| Welcome to the ASU scripts! |")
+    print("-------------------------------")
+
     print("\n[1] Use class code to check availability"
-          "\n[2] Check for all available classes for a course\n")
+          "\n[2] Check for all available classes for a course"
+          "\n[3] View your set alerts\n")
+
     while True:
         selected_input = input("Please select an option: ")
         try:
@@ -18,6 +24,7 @@ def print_menu():
             print("\nSorry, please enter a valid number.")
             continue
 
+    print(f"Selected option[{selected_option}]")
     return selected_option
 
 
@@ -25,19 +32,17 @@ def get_class_code():
     pass
 
 
-def get_course_info():
-    print("Select a semester to search")
-
-    today = datetime.today()
-    current_month = today.month
-    current_year = today.year
-
+# handles the user input for getting the search semester
+def get_semester(current_month, current_year):
     print("Registrations are open for the following semesters:")
+    selected_option = -1
+
     # print available semesters
     if current_month in range(2, 4):  # Feb - Mar
         print(f"[1] Spring {current_year}")  # show current year
         print(f"[2] Summer {current_year}")
         print(f"[3] Fall {current_year}")
+        print()
 
         while True:
             selected_input = input("Please select a semester: ")
@@ -50,10 +55,10 @@ def get_course_info():
             except:
                 print("\nSorry, please enter a valid number.")
                 continue
-
     elif current_month in range(4, 6):  # Apr - May
         print(f"[1] Summer {current_year}")
         print(f"[2] Fall {current_year}")
+        print()
 
         while True:
             selected_input = input("Please select a semester: ")
@@ -66,10 +71,10 @@ def get_course_info():
             except:
                 print("\nSorry, please enter a valid number.")
                 continue
-
     elif current_month in range(9, 11):  # Sep - Oct
         print(f"[1] Fall {current_year}")
         print(f"[2] Spring {current_year + 1}")  # show next year
+        print()
 
         while True:
             selected_input = input("Please select a semester: ")
@@ -82,9 +87,9 @@ def get_course_info():
             except:
                 print("\nSorry, please enter a valid number.")
                 continue
-
     elif current_month in range(11, 13):  # Nov - Dec
         print(f"[1] Spring {current_year + 1}")  # show next year
+        print()
 
         while True:
             selected_input = input("Please select a semester: ")
@@ -97,9 +102,9 @@ def get_course_info():
             except:
                 print("\nSorry, please enter a valid number.")
                 continue
-
     elif current_month == 1:  # Jan
         print(f"[1] Spring {current_year}")  # show current year
+        print()
 
         while True:
             selected_input = input("Please select a semester: ")
@@ -112,9 +117,9 @@ def get_course_info():
             except:
                 print("\nSorry, please enter a valid number.")
                 continue
-
     else:  # rest of the months: Jun - Aug
         print(f"[1] Fall {current_year}")
+        print()
 
         while True:
             selected_input = input("Please select a semester: ")
@@ -127,6 +132,51 @@ def get_course_info():
             except:
                 print("\nSorry, please enter a valid number.")
                 continue
+
+    return selected_option
+
+
+# handles the user input for getting the search subject
+def get_subject():
+    print()
+    while True:
+        subject_input = input("Enter a subject to search (like CSE or MAT): ")
+        if len(subject_input) is 3:
+            return subject_input.upper()
+        else:
+            print("\nSorry, please enter a valid subject code.")
+
+
+# handles the user input for getting the course number
+def get_course_number():
+    print()
+    while True:
+        number_input = input("Enter the course number to search (like 101 or 360): ")
+        try:
+            if len(number_input) is 3 and int(number_input) > 0:
+                return int(number_input)
+            else:
+                print("\nSorry, please enter a valid course number.")
+        except:
+            print("\nSorry, please enter a valid course number.")
+            continue
+
+
+def get_course_info():
+    today = datetime.today()
+    current_month = today.month
+    current_year = today.year
+
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("\nSelect a semester to search")
+    semester = get_semester(current_month, current_year)
+    print(f"Selected semester")
+
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    get_subject()
+
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    get_course_number()
 
 
 if __name__ == '__main__':
