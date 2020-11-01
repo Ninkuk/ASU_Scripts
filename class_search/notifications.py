f1472628e78886b5e02eb6615861dedd4035ba19
Bader Alrifai <bedrrifai1@gmail.com>
2020-10-31 21:24:54 -0700

    search_class_code.py checks if a single class is open and sends a notification if so

--- class_search/notifications.py
+++ class_search/notifications.py
@@ -1,7 +1,7 @@
 from spontit import SpontitResource
 
 
-def getSpontitResource():
+def get_spontit_resource():
     lines = open("username_and_key.txt").read().splitlines()
     resource = SpontitResource(lines[0], lines[1])
     return resource
@@ -9,8 +9,8 @@ def getSpontitResource():
 
 # if push_to_followers is None, it will send it to everybody
 # making the parameter specific_followers default value None does not work and sends nothing.
-def sendNotification(notification_title, notification_text, content, specific_followers=[]):
-    resource = getSpontitResource()
+def send_notification(notification_title, notification_text, content, specific_followers=[]):
+    resource = get_spontit_resource()
 
     if not specific_followers:
         response = resource.push(content, notification_text, notification_title, push_to_followers=None)
